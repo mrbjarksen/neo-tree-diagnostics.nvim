3bc2d86e92e509565cbf69b6bbadb03985fb20da
Bjarki Baldursson Harksen <bjarki31@gmail.com>
2023-03-21 13:03:47 +0000

    feat: add `refresh.delay` and `refresh.event` config options (#23)

--- lua/neo-tree/sources/diagnostics/defaults.lua
+++ lua/neo-tree/sources/diagnostics/defaults.lua
@@ -8,11 +8,11 @@ local config = {
     event = "neo_tree_buffer_enter", -- The event to enable auto preview upon (for example `"neo_tree_window_after_open"`)
   },
   bind_to_cwd = true,
-  diag_sort_function = "severity", -- "severity" means diagnostic items are sorted by severity in addition to their positions.
-                                   -- "position" means diagnostic items are sorted strictly by their positions.
-                                   -- May also be a function.
+  diag_sort_function = "severity", -- "severity" means diagnostic items are sorted by severity in addition to their positions
+                                   -- "position" means diagnostic items are sorted strictly by their positions
+                                   -- May also be a function
   follow_behavior = { -- Behavior when `follow_current_file` is true
-    always_focus_file = false, -- Focus the followed file, even when focus is currently on a diagnostic item belonging to that file.
+    always_focus_file = false, -- Focus the followed file, even when focus is currently on a diagnostic item belonging to that file
     expand_followed = true, -- Ensure the node of the followed file is expanded
     collapse_others = true, -- Ensure other nodes are collapsed
   },
@@ -20,6 +20,11 @@ local config = {
   group_dirs_and_files = true, -- when true, empty folders and files will be grouped together
   group_empty_dirs = true, -- when true, empty directories will be grouped together
   show_unloaded = true, -- show diagnostics from unloaded buffers
+  refresh = {
+    delay = 100, -- Time (in ms) to wait before updating diagnostics. Might resolve some issues with Neovim hanging.
+    event = "vim_diagnostic_changed", -- Event to use for updating diagnostics (for example `"neo_tree_buffer_enter"`)
+                                      -- Set to `false` or `"none"` to disable automatic refreshing
+  },
   renderers = {
     file = {
       { "indent" },
